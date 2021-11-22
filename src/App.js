dc55579e8b0f4b5bcfb7c026b5cf93517e13a216
Ephie Oyedoh <ephieoyedoh@Ephies-MacBook-Pro-2.local>
2021-11-22 10:54:16 +0000

    updating readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import pic1 from './images/pic1.jpeg'
+import pic2 from './images/pic2.jpeg'
+import pic3 from './images/pic3.gif'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src={pic1} className="App-logo" alt="logo" />
+        <img src={pic3} className="App-logo" alt="logo" />
+        <img src={pic2} className="App-logo" alt="logo" />
     </div>
   );
 }
